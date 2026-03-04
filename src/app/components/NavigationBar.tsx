52e0a02bc9bc326f86be9a9a2cfcbdfa08780172
Centhor <vincenthizon266@gmail.com>
2026-03-04 10:22:07 +0800

    fix: odd tab sizing on this file, changed. /Login wrapped by a <List><List></List</List> fixed.

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -10,22 +10,22 @@ const NavigationBar = () => {
     const pathname = usePathname()
 
     return (
-        <div className="min-w-[12vw] bg-card border-r-2 border-card-border flex flex-col justify-between items-center pt-26 pb-8 h-full fixed top-0 left-0">
-                <div>
-                        <List>
-                                <ListItem isActive={pathname === '/dashboard'}><Link href="/dashboard" className="block">Dashboard</Link></ListItem>
-                                <ListItem isActive={pathname === '/trips'}><Link href="/trips" className="block">Trips</Link></ListItem>
-                                <ListItem isActive={pathname === '/expenses'}><Link href="/expenses" className="block">Expenses</Link></ListItem>
-                                <ListItem isActive={pathname === '/analytics'}><Link href="/analytics" className="block">Analytics</Link></ListItem>
-                                <ListItem isActive={pathname === '/drivers'}><Link href="/drivers" className="block">Drivers</Link></ListItem>
-                        </List>
-                </div>
-                <div>
-                        <List>
-                                <List><Link href="/login">Logout</Link></List>
-                        </List>
-                </div>
-        </div>
+	<div className="min-w-[12vw] bg-card border-r-2 border-card-border flex flex-col justify-between items-center pt-26 pb-8 h-full fixed top-0 left-0">
+		<div>
+			<List>
+				<ListItem isActive={pathname === '/dashboard'}><Link href="/dashboard" className="block">Dashboard</Link></ListItem>
+				<ListItem isActive={pathname === '/trips'}><Link href="/trips" className="block">Trips</Link></ListItem>
+				<ListItem isActive={pathname === '/expenses'}><Link href="/expenses" className="block">Expenses</Link></ListItem>
+				<ListItem isActive={pathname === '/analytics'}><Link href="/analytics" className="block">Analytics</Link></ListItem>
+				<ListItem isActive={pathname === '/drivers'}><Link href="/drivers" className="block">Drivers</Link></ListItem>
+			</List>
+		</div>
+		<div>
+			<List>
+				<List><Link href="/login">Logout</Link></List>
+			</List>
+		</div>
+	</div>
     )
 }
 
