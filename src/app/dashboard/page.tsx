9393891ffe8ad516a6c7fd7737d092d437fad12b
Centhor <vincenthizon266@gmail.com>
2026-03-04 10:22:53 +0800

    feat: chart impl @ dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Chart, { DataPoint, ExpenseType } from '../components/Chart'
 import Card from '../components/Card'
 import Image from 'next/image'
 import CardRowContainer from '../components/CardRowContainer'
@@ -11,7 +12,14 @@ const DashboardPage = () => {
   const [totalExpenses, setTotalExpenses] = useState(0);
   const [maintenanceOrders, setMaintenanceOrders] = useState(0);
 
-
+  const [expensesData, setExpensesData] = useState<DataPoint[]>([ //dummy data, replace with api call
+    { name: ExpenseType.MISC, value: 30 },
+    { name: ExpenseType.MAINTENANCE, value: 45 },
+    { name: ExpenseType.TOLLS, value: 45 },
+    { name: ExpenseType.SALARY, value: 28 },
+    { name: ExpenseType.FUEL, value: 28 },
+  ]);
+  
   return (
     <div className="flex flex-col gap-6 justify-between">
 
@@ -41,6 +49,10 @@ const DashboardPage = () => {
 
       <CardRowContainer>
         <Card>
+          <h2>Expenses Chart</h2>
+          <Chart data={expensesData} />
+        </Card>
+        <Card>
           <h2>Current Trips</h2>
         </Card>
         <Card>
@@ -53,36 +65,3 @@ const DashboardPage = () => {
 }
 
 export default DashboardPage
-
-{/* <Card title="Total Trips" height={100} width={275}> 
-  <div className="flex items-center gap-2 relative h-full">
-    <Image src="/package.svg" alt="package" width={24} height={24} />
-    <h1 className="text-3xl font-bold">240</h1>
-    <div className="absolute bottom-3 right-1 flex gap-1">
-      <Image src="/arrow.svg" alt="arrow" width={16} height={16} />
-      <span className="text-lg font-semibold">+12%</span>
-    </div>
-  </div>
-</Card>
-<Card title="Total Expenses" height={100} width={300}>
-  <div className="flex items-center gap-2 relative h-full">
-    <Image src="/money.svg" alt="money" width={30} height={30} />
-    <h1 className="text-2xl font-bold">P22,400.00</h1>
-    <div className="absolute bottom-3 right-1 flex gap-1">
-      <Image src="/arrow.svg" alt="arrow" width={16} height={16} />
-      <span className="text-lg font-semibold">+12%</span>
-    </div>
-  </div>
-</Card>
-<Card title="Maintainance Orders" height={100} width={275}>
-  <div className="flex items-center gap-2 relative h-full">
-    <Image src="/setting.svg" alt="setting" width={24} height={24} />
-    <h1 className="text-3xl font-bold">9</h1>
-  </div>
-</Card>
-<Card title="Active Drivers" height={100} width={275}>
-  <div className="flex items-center gap-2 relative h-full">
-    <Image src="/package_car.svg" alt="package_car" width={24} height={24} />
-    <h1 className="text-3xl font-bold">240</h1>
-  </div>
-</Card> */}
