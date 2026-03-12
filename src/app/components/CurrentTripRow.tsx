35322011dc1aa1930471d123eeccd2ff25c7e27d
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-12 23:35:11 +0800

    feat: add CurrentTripRow

---
+++ src/app/components/CurrentTripRow.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CurrentTripRow = () => {
+  return (
+    <div>
+        <div>
+            <div className="bg-blue-300 size-8 rounded-4xl"></div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default CurrentTripRow
