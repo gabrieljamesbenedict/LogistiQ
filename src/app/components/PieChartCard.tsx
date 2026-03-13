c1121d9a3db929a1e541d3856f6132dd3282bde0
Gabriel James Benedict <benok.loslos@gmail.com>
2026-03-13 15:42:12 +0800

    refactor: rename to PieChartCard for clarity

--- src/app/components/PieChartCard.tsx
+++ src/app/components/PieChartCard.tsx
@@ -3,26 +3,20 @@ import React from 'react';
 import { PieChart, Pie, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 export interface DataPoint{
-  name: ExpenseType;
+  name: string;
   value: number;
-}
-
-export enum ExpenseType {
-  MISC = 'Misc',
-  MAINTENANCE = 'Maintenance',
-  FUEL = 'Fuel',
-  TOLLS = 'Tolls',
-  SALARY = 'Salary'
 }
 
 const COLORS = ['#4f46e5', '#10b981', '#f59e0b', '#ef4444', '#3b82f6'];
 
-export default function ChartCard({ data }: {data : DataPoint[]}) {
+export default function PieChartCard({ data }: {data : DataPoint[]}) {
+  
   const dataFormatted = data.map((item, index) => ({
     ...item, fill: COLORS[index]
   }));
+  
   return(
-    <ResponsiveContainer width="100%" height="100%" minHeight={300}>
+    <ResponsiveContainer width="100%" height="100%">
       <PieChart margin={{top:30,bottom:30}}>
         <Pie
           data={dataFormatted}
